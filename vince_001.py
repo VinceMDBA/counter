86c281866aae2a2617ea80cb62cc70751e21ede8
Vince <vincentjoakley@gmail.com>
2018-11-30 14:25:28 +1100

    test commit

---
+++ vince_001.py
@@ -0,0 +1,10 @@
+def main():
+    count = 1
+    while count <= 20:
+        print ("Count: %s" % count)
+        count = count + 1
+
+if __name__ == '__main__':
+    main()
+    print("Program complete")
+    
